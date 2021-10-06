f3274001d15f1f5f119e755f65a4c48ddbb9ba4f
Linda Kounechongprasert <lindak@Lindas-MacBook-Pro.local>
2021-10-06 11:18:13 -0700

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("testing");
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Promt user to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (err) {
+        //Catch error here
+        console.log('Uh oh! Error: ', err);
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    //Resets Button
+    button.disabled = false;
+})
+
+// On Load
+selectMediaStream();
